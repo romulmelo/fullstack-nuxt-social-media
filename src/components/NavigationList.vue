a21ac5eab8a5f204ffa6b2e141e5708cdf8151bd
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-26 14:30:07 -0300

    feat: add navigation-list component

---
+++ src/components/NavigationList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+const items = [
+  {
+    to: "/",
+    text: "Home",
+    icon: "home"
+  },
+  {
+    text: "Explore",
+    icon: "hashtag"
+  },
+  {
+    text: "Contact",
+    icon: "bell"
+  },
+  {
+    text: "Messages",
+    icon: "mail"
+  },
+  {
+    text: "Bookmarks",
+    icon: "bookmark"
+  },
+  {
+    text: "Lists",
+    icon: "document-text"
+  },
+  {
+    text: "Profile",
+    icon: "user"
+  }
+]
+</script>
+
+<template>
+  <ul class="flex flex-col">
+    <NavigationLink
+      v-for="item in items"
+      :key="item.text"
+      :to="item.to"
+      :text="item.text"
+      :icon="item.icon"
+    />
+  </ul>
+</template>
