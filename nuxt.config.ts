5cf24cc2040b75d0265536fddabd48e46be6dffa
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-26 11:03:22 -0300

    feat: add custom configuration to @nuxtjs/color-mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { colorModeModule } from "./src/config/modules/color-mode-module"
 import { googleFontsModule } from "./src/config/modules/google-fonts-module"
 
 export default defineNuxtConfig({
   srcDir: "src",
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/google-fonts"
+  ],
   // config for @nuxtjs/google-fonts
-  googleFonts: googleFontsModule.googleFonts
+  googleFonts: googleFontsModule.googleFonts,
+  // config for @nuxtjs/color-mode
+  colorMode: colorModeModule.colorMode
 })
---
+++ src/config/modules/color-mode-module.ts
@@ -0,0 +1,10 @@
+const config = {
+  colorMode: {
+    preference: "system",
+    fallback: "dark",
+    classSuffix: "",
+    storageKey: "theme"
+  }
+}
+
+export { config as colorModeModule }
