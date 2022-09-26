adfb6f564810f6d8931c47f816006fabbfb3186f
Rômulo Melo <dev.eu.romulo.melo@gmail.com>
2022-09-26 14:29:47 -0300

    feat: add navigation-link component

---
+++ src/components/NavigationLink.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  to?: string
+  text: string
+  icon?: string
+}>()
+</script>
+
+<template>
+  <li class="flex-1 py-1">
+    <nuxt-link :to="to" class="block h-full" :aria-label="text">
+      <div
+        class="flex w-min flex-col items-center justify-center rounded-full p-3 transition-colors hover:bg-black/10 dark:hover:bg-dim-200 lg:flex-row lg:justify-start"
+      >
+        <Icon
+          :name="'heroicons-outline:' + icon"
+          size="32"
+          class="text-dark dark:text-white"
+        />
+        <span class="ml-5 mr-4 hidden text-xl dark:text-white lg:block">
+          {{ text }}
+        </span>
+      </div>
+    </nuxt-link>
+  </li>
+</template>
+
+<style scoped>
+a.active,
+a.exact {
+  @apply font-bold;
+}
+</style>
